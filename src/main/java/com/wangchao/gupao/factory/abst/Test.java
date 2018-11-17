114fb4c06dc7ce7100aa722a0c7b08722007fced
Administrator <244067166@qq.com>
2018-11-17 22:38:05 +0800

    咕泡工厂模式

--- src/main/java/com/wangchao/gupao/factory/abst/Test.java
+++ src/main/java/com/wangchao/gupao/factory/abst/Test.java
@@ -4,7 +4,6 @@ import com.wangchao.gupao.factory.function.MengNiuFactory;
 
 public class Test {
     public static void main(String[] args) {
-
         AbstractFactory factory = new MilkFactory();
         System.out.println(factory.getMengNiu().getName());
     }
